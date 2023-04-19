eba377230107c758ccb20ba71d71e281127f89a7
Cole <122782745+cm-thom@users.noreply.github.com>
2023-04-19 13:10:06 -0400

    added code for deployment

---
+++ script.js
@@ -0,0 +1,73 @@
+authBtnEl = document.getElementById('auth-btn');
+
+const clientId = 'ca2ce6aeb1fa483bb8544ccdc4ece44c';
+const redirectUri = 'https://localhost:8080';
+
+authBtnEl.addEventListener('click', function() {
+
+    let codeVerifier = generateRandomString(128);
+
+    generateCodeChallenge(codeVerifier).then(codeChallenge => {
+    let state = generateRandomString(16);
+    let scope = 'user-read-private user-read-email';
+
+    localStorage.setItem('code_verifier', codeVerifier);
+
+  let args = new URLSearchParams({
+    response_type: 'code',
+    client_id: clientId,
+    scope: scope,
+    redirect_uri: redirectUri,
+    state: state,
+    code_challenge_method: 'S256',
+    code_challenge: codeChallenge
+  });
+
+  window.location = 'https://accounts.spotify.com/authorize?' + args;
+});
+});
+
+
+
+function generateRandomString(length) {
+    let text = '';
+    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  
+    for (let i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    console.log(text);
+    return text;
+  }
+
+  async function generateCodeChallenge(codeVerifier) {
+    function base64encode(string) {
+      return btoa(String.fromCharCode.apply(null, new Uint8Array(string)))
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_')
+        .replace(/=+$/, '');
+    }
+  
+    const encoder = new TextEncoder();
+    const data = encoder.encode(codeVerifier);
+    const digest = await window.crypto.subtle.digest('SHA-256', data);
+  
+    return base64encode(digest);
+  }
+
+  
+
+const urlParams = new URLSearchParams(window.location.search);
+let code = urlParams.get('code');
+
+
+let codeVerifier = localStorage.getItem('code_verifier');
+
+let body = new URLSearchParams({
+  grant_type: 'authorization_code',
+  code: code,
+  redirect_uri: redirectUri,
+  client_id: clientId,
+  code_verifier: codeVerifier
+});
+
